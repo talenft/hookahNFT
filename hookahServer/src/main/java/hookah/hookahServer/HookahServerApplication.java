2256b36d27371c68556d6807bec232dfc5f3fbf3
unknown <you@example.com>
2021-09-28 10:54:36 +0300

    feat(server side):add server side folder

---
+++ hookahServer/src/main/java/hookah/hookahServer/HookahServerApplication.java
@@ -0,0 +1,13 @@
+package hookah.hookahServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HookahServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HookahServerApplication.class, args);
+	}
+
+}
